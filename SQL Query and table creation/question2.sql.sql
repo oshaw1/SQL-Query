f54d54069e9a1e5973dded7ad4d49d00b943939f
Owen Shaw <123586721+oshaw1@users.noreply.github.com>
2023-07-26 11:38:23 +0100

    Unzipped the sql queries for github

---
+++ SQL Query and table creation/question2.sql.sql
@@ -0,0 +1,10 @@
+INSERT INTO Customers (CustomerID, CustomerName, Customeraddress, Customerpaymenttype, Customerpaymentmethod, Dateofmembershipstart, Pettype)
+VALUES ('1', 'Owen Shaw', '1 example st', 'Payment plan', 'Debit', '12/11/2020', 'cat');
+INSERT INTO Customers (CustomerID, CustomerName, Customeraddress, Customerpaymenttype, Customerpaymentmethod, Dateofmembershipstart, Pettype)
+VALUES ('2', 'John smith', '2 example st', 'Payment plan', 'Debit', '12/11/2020', 'Dog');
+INSERT INTO Customers (CustomerID, CustomerName, Customeraddress, Customerpaymenttype, Customerpaymentmethod, Dateofmembershipstart, Pettype)
+VALUES ('3', 'Tom Scott', '5 example st', 'Payment plan', 'Debit', '12/11/2020', 'cat');
+INSERT INTO Customers (CustomerID, CustomerName, Customeraddress, Customerpaymenttype, Customerpaymentmethod, Dateofmembershipstart, Pettype)
+VALUES ('4', 'Debby Mills', '3 example st', 'Payment plan', 'Debit', '12/11/2020', 'Cat');
+INSERT INTO Customers (CustomerID, CustomerName, Customeraddress, Customerpaymenttype, Customerpaymentmethod, Dateofmembershipstart, Pettype)
+VALUES ('5', 'Danny Thomas', '4 example st', 'Payment plan', 'Debit', '12/11/2020', 'Cat');
---
+++ SQL Query and table creation/question3.sql.sql
@@ -0,0 +1,1 @@
+SELECT Customername FROM Customers;
